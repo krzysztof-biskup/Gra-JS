beb4a34dbc28f91064cdcda1be17e190b26fe01c
Krzysiek <krzysiek@popracy.eu>
2019-09-24 11:39:02 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,158 +1,136 @@
 'use strict';
 
 var params = {
-  gameNumber: 0,
+  roundsNumber: 1,
   moveNumber: 0,
   compWin: 0,
   userWin: 0,
-  rock: 'rock',
-  paper: 'paper',
-  scissors: 'scissors',
+  computerMoves: ['rock', 'paper', 'scissors'],
+  progress: [],
+  roundNumber: 1,
 }
 
-var buttons = document.querySelectorAll('.player-move');
-
-document.getElementById("newGameButton").addEventListener('click', games);
+document.getElementById('newGameButton').addEventListener('click', games);
 var roundNumber = document.getElementById('roundNumber');
 var compWin_field = document.getElementById('compWin_field');
 var userWin_field = document.getElementById('userWin_field');
 
-function games(){
+function games() {
   reset();
-  params.gameNumber = window.prompt('Liczba wygranych rund które kończą gre?');
-  roundNumber.innerHTML = params.gameNumber;
+  params.roundsNumber = window.prompt('Liczba wygranych rund które kończą gre?');
+  roundNumber.innerHTML = params.roundsNumber;
 };
 
-var computerPick = function randomNumber(min, max){
-  min = Math.ceil(1);
-  max = Math.floor(3);
+var computerPick = function randomNumber(min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(2);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+var game = function (playerMove, computerMove,) {
+  if (playerMove == computerMove) {
+    output.innerHTML = 'Remis' + '<br><br>';
+  } else if (playerMove == 'rock') {
+    if (computerMove == 'paper') {
+      output.innerHTML = 'YOU LOOSE: you played ' + playerMove + ',' + ' computer played ' + computerMove + '<br><br>';
+      params.compWin++;
+    } else {
+      output.innerHTML = 'You WON: you played ' + playerMove + ',' + ' computer played ' + computerMove + '<br><br>';
+      params.userWin++;
+    }
+  } else if (playerMove == 'paper') {
+    if (computerMove == 'scissors') {
+      output.innerHTML = 'YOU LOOSE: you played ' + playerMove + ',' + ' computer played ' + computerMove + '<br><br>';
+      params.compWin++;
+    } else {
+      output.innerHTML = 'You WON: you played ' + playerMove + ',' + ' computer played ' + computerMove + '<br><br>';
+      params.userWin++;
+    }
+  } else if (playerMove == 'scissors') {
+    if (computerMove == 'rock') {
+      output.innerHTML = 'YOU LOOSE: you played ' + playerMove + ',' + ' computer played ' + computerMove + '<br><br>';
+      params.compWin++;
+    } else {
+      output.innerHTML = 'You WON: you played ' + playerMove + ',' + ' computer played ' + computerMove + '<br><br>';
+      params.userWin++;
+    }
+  }
+  showModal();
+  params.progress.push({
+    roundNumber: params.roundNumber++,
+    roundResult: output.innerHTML, 
+    computerMove: computerMove,
+    playerMove: playerMove,
+    userWin: params.userWin,
+    compWin: params.compWin,
+  })
 
-var comPickNames = function(comPick){ //funkcja przypisywania nazwy wybranej pozycji przez komputer
-  if (comPick == 1) {
-    var name = 'Rock';
-    return name;
+if (params.roundsNumber != params.moveNumber) {
+  for (var i = 0; i < params.progress.length; i++) 
+  {
+    modalContent.innerHTML = 
+      'Numer rundy ' + params.progress[i]['roundNumber'] + '<br><br>' + 
+      'Ruch gracza: ' + params.progress[i]['playerMove'] + '<br><br>' + 
+      'Ruch komputera: ' + params.progress[i]['computerMove'] + '<br><br>' +
+      'Wynik rundy: ' + params.progress[i]['roundResult'] +
+      'Wynik gry po tej rundzie: ' + 'User: ' + params.progress[i]['userWin'] + ' vs ' + 'Computer: ' + params.progress[i]['compWin'];   
   } 
-  else if (comPick == 2) {
-    var name = 'Paper';
-    return name;
+} else {
+  var j = params.roundsNumber - 1;
+  modalContent.innerHTML = 'Wynik gry: ' + 'User: ' + params.progress[j]['userWin'] + ' vs ' + 'Komputer: ' + params.progress[j]['compWin'] + '<br>';
+  if (params.progress[j]['userWin'] < params.progress[j]['compWin']) {
+    modalContent.innerHTML += 'Komputer wygrał'
+  }  else {
+    modalContent.innerHTML += 'User wygrał'
   }
-  else {
-    var name = 'Scissors';
-    return name;
-  }
+}
 };
 
+var buttons = document.getElementsByClassName('player-move');
 
-var game = function(comPick, userPick, userPickName, comPickName) {
-  if (comPick == 1 && (userPick == 1 || userPick == 3)) {   
-    output.innerHTML = 'YOU LOOSE: you played ' + userPickName + ',' + ' computer played ' + comPickName + '<br><br>';//comp win
-    params.compWin++;
-   
-    compWin_field.innerHTML = 'Comp win: ' + params.compWin;
-    userWin_field.innerHTML = 'User win: ' + params.userWin;
+for (var button of buttons) {
+  button.addEventListener('click', function () {
+    var playerMove = this.getAttribute('data-move');
+    var index = computerPick();
+    var computerMove = params.computerMoves[index]
+    params.moveNumber++;
+    game(playerMove, computerMove);
+    buttonDisable(params.roundsNumber == params.moveNumber);
 
-  } 
-  else if (comPick == 2 && (userPick == 1 || userPick == 2)) {
-    output.innerHTML = 'YOU LOOSE: you played ' + userPickName + ',' + ' computer played ' + comPickName + '<br><br>';//comp win
-    params.compWin++;
-    
-    compWin_field.innerHTML = 'Comp win: ' + params.compWin;
-    userWin_field.innerHTML = 'User win: ' + params.userWin;
-    
-  }
-  else if (comPick == 3 && (userPick == 2 || userPick == 3)) {    
-    output.innerHTML = 'YOU LOOSE: you played ' + userPickName + ',' + ' computer played ' + comPickName + '<br><br>';//comp win
-    params.compWin++;
-    
-    compWin_field.innerHTML = 'Comp win: ' + params.compWin;
-    userWin_field.innerHTML = 'User win: ' + params.userWin;
-    
-  }
-  else {
-    output.innerHTML = 'You WON: you played ' + userPickName + ',' + ' computer played ' + comPickName + '<br><br>';//you won
-    params.userWin++;
-    
-    compWin_field.innerHTML = 'Comp win: ' + params.compWin;
-    userWin_field.innerHTML = 'User win: ' + params.userWin;
-  } 
+  });
 };
 
-playerMove_rock.addEventListener('click', function(){
-  var rock = 1;
-  var pickRock = 'Rock';
-  
-  var comPick = computerPick();
-  var comPickName = comPickNames(comPick); //przypisywanie nazwy wybranej pozycji przez komputer
-  params.moveNumber++;
-  
-  game(comPick, rock, pickRock, comPickName, params);
-  buttonDisable(params.gameNumber == params.moveNumber);
-  
-});
-
-playerMove_paper.addEventListener('click', function(){
-  var paper = 2;
-  var pickPaper = 'Paper';
-  params.moveNumber++;
-  
-  var comPick = computerPick();
-  var comPickName = comPickNames(comPick); //przypisywanie nazwy wybranej pozycji przez komputer
-  
-  game(comPick, paper, pickPaper, comPickName);
-  buttonDisable(params.gameNumber == params.moveNumber);
-  
-});
-
-playerMove_scissors.addEventListener('click', function(){
-  var scissors = 3;
-  var pickScissors = 'Scissors';
-  params.moveNumber++;
-  
-  var comPick = computerPick();
-  var comPickName = comPickNames(comPick); //przypisywanie nazwy wybranej pozycji przez komputer
-  
-  game(comPick, scissors, pickScissors, comPickName);
-  buttonDisable(params.gameNumber == params.moveNumber);
-
-  
-});
- 
-var buttonDisable = function(toogle) {
+var buttonDisable = function (toogle) {
   document.getElementById("playerMove_scissors").disabled = toogle;
   document.getElementById("playerMove_paper").disabled = toogle;
   document.getElementById("playerMove_rock").disabled = toogle;
-  
-}; 
-  
-var reset = function() {
+};
+
+var reset = function () {
   buttonDisable(false);
   params.moveNumber = 0;
-  params.gameNumber = 0;
-  
+  params.roundNumber = 1;
+
   params.compWin = 0;
   params.userWin = 0;
-  
+
   compWin_field.innerHTML = params.compWin;
   userWin_field.innerHTML = params.userWin;
 };
- 
 
-var showModal = function(){
+
+var showModal = function () {
   event.preventDefault();
   document.querySelector('#modal-overlay').classList.add('show');
-  
   document.getElementById('modal-one').classList.add('show');
 };
 
-var hideModal = function(event){
+var hideModal = function (event) {
   event.preventDefault();
   document.querySelector('#modal-overlay').classList.remove('show');
-  
+
   var modalsHide = document.querySelectorAll('.modal');
-  
+
   for (var p = 0; p < modalsHide.length; p++) {
     modalsHide[p].classList.remove('show');
   }
@@ -160,10 +138,14 @@ var hideModal = function(event){
 
 var closeButtons = document.querySelectorAll('.modal .close');
 
-for(var i = 0; i < closeButtons.length; i++){
+for (var i = 0; i < closeButtons.length; i++) {
   closeButtons[i].addEventListener('click', hideModal);
 }
 
-// Dobrą praktyką jest również umożliwianie zamykania modala poprzez kliknięcie w overlay. 
-
 document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+
+var modalContent = document.querySelector('.content');
+
+
+
+
